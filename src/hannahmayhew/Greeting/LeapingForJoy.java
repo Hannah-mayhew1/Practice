68f66e68f91248c7881479d6e2057525ca4fa26e
Hannah.Mayhew <hannah.mayhew@HannahMayhews-MacBook-Pro.local>
2019-03-01 14:10:08 +0000

    LeapingForJoy

---
+++ src/hannahmayhew/Greeting/LeapingForJoy.java
@@ -0,0 +1,33 @@
+package hannahmayhew.Greeting;
+
+import java.util.Scanner;
+
+public class LeapingForJoy {
+
+    public static void main(String[] args) {
+        System.out.println("Please enter a number:");
+
+        Scanner scanner = new Scanner(System.in);
+
+        if (scanner.hasNextInt()) {
+            int input = scanner.nextInt();
+
+            int yearsFound = 0;
+
+            for (int year = 2019; yearsFound != input; year++) {
+                if ((year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0)) {
+                    yearsFound = yearsFound + 1;
+                    System.out.println(year);
+
+                } else if ((year % 4 == 0) && (year % 100 != 0)) {
+                    yearsFound = yearsFound + 1;
+                    System.out.println(year);
+                }
+
+
+            }
+
+        }
+    }
+}
+
